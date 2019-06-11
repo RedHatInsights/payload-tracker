411fcfc5e383ce62ddb583aa6a0e6c9e767695c1
LI0131 <mccannliam.lm@gmail.com>
2019-06-11 12:44:50 -0400

    Added date and created_at filters

--- api/stats.py
+++ api/stats.py
@@ -1,3 +1,6 @@
+import operator
+from dateutil import parser
+
 import responses
 from db import db, Payload
 
@@ -7,26 +10,44 @@ async def search(*args, **kwargs):
     stat_funcs = {"count": _count, "percentage": _percentage}
     basic_eq_filters = ['status', 'service', 'inventory_id', 'account',
                         'source', 'system_id', 'status_msg']
+    date_group_fns = {'_lt': operator.lt, '_lte': operator.le,
+                      '_gt': operator.gt, '_gte': operator.ge}
     func_params = {}
 
     func = stat_funcs[kwargs['stat']]
     func_params['set_size'] = await db.func.count(getattr(Payload, "id")).gino.scalar()
 
     if 'set_constraint' in kwargs:
+        constraint_query = Payload.query
         set_constraint_param = kwargs['set_constraint']
-        basic_eq_filters.remove(set_constraint_param)
-        if set_constraint_param in kwargs:
+        if set_constraint_param in ['date', 'created_at']:
+            for date_group_str, date_group_fn in date_group_fns.items():
+                if set_constraint_param + date_group_str in kwargs:
+                    the_date = parser.parse(kwargs[set_constraint_param + date_group_str])
+                    constraint_query.append_whereclause(
+                        date_group_fn(getattr(Payload, set_constraint_param), the_date)
+                    )
+        elif set_constraint_param in kwargs:
             set_constraint_value = kwargs[set_constraint_param]
-            set_size = await db.func.count(
+            constraint_query.append_whereclause(
                 getattr(Payload, set_constraint_param) == set_constraint_value
-            ).gino.scalar()
-            func_params['set_size'] = set_size
+            )
+        payload_dump = [payload.dump() for payload in await constraint_query.gino.all()]
+        func_params['set_size'] = len(payload_dump)
 
     for search_param_key in kwargs:
         if search_param_key in basic_eq_filters:
             search_param_value = kwargs[search_param_key]
             payload_query.append_whereclause(
                 getattr(Payload, search_param_key) == search_param_value)
+
+    for date_field in ['date', 'created_at']:
+        for date_group_str, date_group_fn in date_group_fns.items():
+            if date_field + date_group_str in kwargs:
+                the_date = parser.parse(kwargs[date_field + date_group_str])
+                payload_query.append_whereclause(
+                    date_group_fn(getattr(Payload, date_field), the_date)
+                )
 
     payloads = await payload_query.gino.all()
     payloads_dump = [payload.dump() for payload in payloads]
