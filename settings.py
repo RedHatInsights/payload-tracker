d921a3ea38899a76e2fc40e050600e16698dad78
Richard Brantley <rbrantle@redhat.com>
2019-06-12 15:49:56 -0400

    Fix DEV LOG LEVEL

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ ENVIRONMENT = os.environ.get('TRACKER_ENV', 'dev')
 DEBUG = ENVIRONMENT in ('dev', 'ci', 'qa')
 
 # Logging settings
-DEV_LOG_LEVEL = 'INFO'
+DEV_LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
 DEV_LOG_MSG_FORMAT = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] " \
             "[%(thread)d  %(threadName)s] [%(process)d] %(message)s"
 DEV_LOG_DATE_FORMAT = "%H:%M:%S"
