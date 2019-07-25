36b640b54ebd8cdb9e8c2131f93ce40d00ae8b8d
Richard Brantley <rbrantle@redhat.com>
2019-07-25 11:01:16 -0400

    Restore 7 day retention policy

--- cron-job.sh
+++ cron-job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DELETE FROM payloads WHERE created_at < (NOW() - interval '2 days');"
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DELETE FROM payloads WHERE created_at < (NOW() - interval '7 days');"
 PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "VACUUM ANALYZE payloads;"
