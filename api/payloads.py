a99edc476004e5acdd940614a0bed53cb804c220
Richard Brantley <rbrantle@redhat.com>
2019-08-01 14:11:58 -0400

    Add elapsed time, use db wrapper to release conn

--- api/payloads.py
+++ api/payloads.py
@@ -4,57 +4,67 @@ import responses
 import operator
 import logging
 import settings
+import time
 
 logger = logging.getLogger(settings.APP_NAME)
 
 
 async def search(*args, **kwargs):
 
+    payloads = None
+    payloads_dump = []
+    # Calculate elapsed time
+    start = time.time()
+
     # initialize connection
-    conn = await db.bind.acquire()
+    async with db.bind.acquire() as conn:
 
-    # Base query
-    payload_query = Payload.query
+        # Base query
+        payload_query = Payload.query
 
-    # These filters are used to filter within the database using equality comparisons
-    basic_eq_filters = ['status', 'service', 'inventory_id', 'account',
-                        'source', 'system_id', 'status_msg']
+        # These filters are used to filter within the database using equality comparisons
+        basic_eq_filters = ['status', 'service', 'inventory_id', 'account',
+                            'source', 'system_id', 'status_msg']
 
-    # Compose where clauses for any of the basic equality filters in the kwargs
-    for search_param_key in kwargs:
-        if search_param_key in basic_eq_filters:
-            search_param_value = kwargs[search_param_key]
-            payload_query.append_whereclause(
-                getattr(Payload, search_param_key) == search_param_value)
-
-    # Perform comparisons on date fields 'date', and 'created_at'
-    date_group_fns = {'_lt': operator.lt, '_lte': operator.le,
-                      '_gt': operator.gt, '_gte': operator.ge}
-    for date_field in ['date', 'created_at']:
-        for date_group_str, date_group_fn in date_group_fns.items():
-            if date_field + date_group_str in kwargs:
-                the_date = parser.parse(kwargs[date_field + date_group_str])
+        # Compose where clauses for any of the basic equality filters in the kwargs
+        for search_param_key in kwargs:
+            if search_param_key in basic_eq_filters:
+                search_param_value = kwargs[search_param_key]
                 payload_query.append_whereclause(
-                    date_group_fn(getattr(Payload, date_field), the_date)
-                )
+                    getattr(Payload, search_param_key) == search_param_value)
 
-    # Get the count before we apply the page size and offset
-    payloads_count = await conn.scalar(payload_query.alias().count())
+        # Perform comparisons on date fields 'date', and 'created_at'
+        date_group_fns = {'_lt': operator.lt, '_lte': operator.le,
+                          '_gt': operator.gt, '_gte': operator.ge}
+        for date_field in ['date', 'created_at']:
+            for date_group_str, date_group_fn in date_group_fns.items():
+                if date_field + date_group_str in kwargs:
+                    the_date = parser.parse(kwargs[date_field + date_group_str])
+                    payload_query.append_whereclause(
+                        date_group_fn(getattr(Payload, date_field), the_date)
+                    )
 
-    # Then apply page size and offset
-    sort_func = getattr(db, kwargs['sort_dir'])
-    payload_query = payload_query.limit(kwargs['page_size']).offset(
-            kwargs['page'] * kwargs['page_size']).order_by(sort_func(kwargs['sort_by']))
+        # Get the count before we apply the page size and offset
+        payloads_count = await conn.scalar(payload_query.alias().count())
 
-    # Compile set of payloads from the database
-    payloads = await conn.all(payload_query)
-    payloads_dump = [payload.dump() for payload in payloads]
+        # Then apply page size and offset
+        sort_func = getattr(db, kwargs['sort_dir'])
+        payload_query = payload_query.limit(kwargs['page_size']).offset(
+                kwargs['page'] * kwargs['page_size']).order_by(sort_func(kwargs['sort_by']))
+
+        # Compile set of payloads from the database
+        payloads = await conn.all(payload_query)
+        payloads_dump = [payload.dump() for payload in payloads]
+
+        # Calculate elapsed time
+        stop = time.time()
+        elapsed = stop - start
 
     # Send results
     if payloads is None:
         return responses.not_found()
     else:
-        return responses.search(payloads_count, payloads_dump)
+        return responses.search(payloads_count, payloads_dump, elapsed)
 
 
 def _get_durations(payloads):
@@ -88,16 +98,19 @@ def _get_durations(payloads):
 
 async def get(payload_id, *args, **kwargs):
 
-    # initialize connection
-    conn = await db.bind.acquire()
+    payloads = None
+    payload_dump = []
 
-    logger.debug(f"Payloads.get({payload_id}, {args}, {kwargs})")
-    sort_func = getattr(db, kwargs['sort_dir'])
-    payloads = await conn.all(Payload.query.where(
-        Payload.payload_id == payload_id
-    ).order_by(
-        sort_func(kwargs['sort_by'])
-    ))
+    # initialize connection
+    async with db.bind.acquire() as conn:
+
+        logger.debug(f"Payloads.get({payload_id}, {args}, {kwargs})")
+        sort_func = getattr(db, kwargs['sort_dir'])
+        payloads = await conn.all(Payload.query.where(
+            Payload.payload_id == payload_id
+        ).order_by(
+            sort_func(kwargs['sort_by'])
+        ))
 
     if payloads is None:
         return responses.not_found()
