c53eb83a9e3079a612b79bfe53d628025e4501b1
Richard Brantley <rbrantle@redhat.com>
2020-05-22 12:16:18 -0400

    Delete payload statuses in cron job

--- cron-job.sh
+++ cron-job.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DELETE FROM payload_statuses WHERE created_at < (NOW() - interval '7 days');"
 PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "DELETE FROM payloads WHERE created_at < (NOW() - interval '7 days');"
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "VACUUM ANALYZE payload_statuses;"
 PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "VACUUM ANALYZE payloads;"
