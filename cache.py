86caeaa9c433a2deb7bb96eaa106af63328705d4
LI0131 <mccannliam.lm@gmail.com>
2020-11-18 16:49:33 -0500

    Add metric for cache request latency

--- cache.py
+++ cache.py
@@ -2,18 +2,34 @@ import os
 import logging
 import settings
 import traceback
+import time
 from redis import Redis
+from prometheus_client import Summary
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
 SECONDS_TO_LIVE = int(os.environ.get('SECONDS_TO_LIVE', 100))
 logger = logging.getLogger(settings.APP_NAME)
+REDIS_LATENCY = Summary('payload_tracker_redis_latency',
+                        'Tracks latency in requests to Redis',
+                        ['request_type'])
 
 
 class Client(Redis):
 
     def __init__(self, **kwargs):
         super(Client, self).__init__(**kwargs)
+
+    def execute_command(self, *args, **options):
+        command_type = args[0]
+        start = time.time()
+        res = super(Client, self).execute_command(*args, **options)
+        duration = time.time() - start
+        if command_type in ['HGETALL', 'LLEN', 'LRANGE']:
+            REDIS_LATENCY.labels(request_type='read').observe(duration)
+        elif command_type in ['HSET', 'LPUSH', 'EXPIRE']:
+            REDIS_LATENCY.labels(request_type='write').observe(duration)
+        return res
 
     def hgetall(self, name, key_is_int=False):
         # Overriding this method to provide decoding
